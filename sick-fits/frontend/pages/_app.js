4708e43f39645b43114ecd91055098cde41f81a2
megafinz <megafinz@gmail.com>
2022-08-26 12:51:41 +0200

    Basic layout components

---
+++ sick-fits/frontend/pages/_app.js
@@ -0,0 +1,17 @@
+/* eslint-disable react/jsx-props-no-spreading */
+
+import PropTypes from 'prop-types';
+import Page from '../components/Page';
+
+export default function App({ Component, pageProps }) {
+  return (
+    <Page>
+      <Component {...pageProps} />
+    </Page>
+  );
+}
+
+App.propTypes = {
+  Component: PropTypes.any,
+  pageProps: PropTypes.any,
+};
