733fbac6c7f687bf06c1280744704747baf019c1
megafinz <megafinz@gmail.com>
2022-08-28 19:39:02 +0200

    Create product form

---
+++ sick-fits/frontend/components/CreateProduct.js
@@ -0,0 +1,106 @@
+import Router from 'next/router';
+import { useMutation } from '@apollo/client';
+import gql from 'graphql-tag';
+import useForm from '../lib/useForm';
+import Form from './styles/Form';
+import DisplayError from './ErrorMessage';
+import { ALL_PRODUCTS_QUERY } from './Products';
+
+const CREATE_PRODUCT_MUTATION = gql`
+  mutation CREATE_PRODUCT_MUTATION(
+    $name: String!
+    $price: Int!
+    $description: String!
+    $image: Upload
+  ) {
+    createProduct(
+      data: {
+        name: $name
+        price: $price
+        description: $description
+        status: "AVAILABLE"
+        photo: { create: { image: $image, altText: $name } }
+      }
+    ) {
+      id
+      name
+      price
+      description
+    }
+  }
+`;
+
+export default function CreateProduct() {
+  const { inputs, handleChange, resetForm, clearForm } = useForm({
+    image: '',
+    name: '',
+    price: '',
+    description: '',
+  });
+  const [createProduct, { loading, error, data }] = useMutation(
+    CREATE_PRODUCT_MUTATION,
+    {
+      variables: inputs,
+      refetchQueries: [ALL_PRODUCTS_QUERY],
+    }
+  );
+  return (
+    <Form
+      onSubmit={async (e) => {
+        e.preventDefault();
+        const res = await createProduct();
+        clearForm();
+        Router.push({
+          pathname: `/product/${res.data.createProduct.id}`,
+        });
+      }}
+    >
+      <DisplayError error={error} />
+      <fieldset disabled={loading} aria-busy={loading}>
+        <label htmlFor="image">
+          Image
+          <input
+            type="file"
+            id="image"
+            name="image"
+            onChange={handleChange}
+            required
+          />
+        </label>
+        <label htmlFor="name">
+          Name
+          <input
+            type="text"
+            id="name"
+            name="name"
+            placeholder="Name"
+            value={inputs.name}
+            onChange={handleChange}
+          />
+        </label>
+        <label htmlFor="price">
+          Price
+          <input
+            type="number"
+            id="price"
+            name="price"
+            placeholder="Price"
+            value={inputs.price}
+            onChange={handleChange}
+          />
+        </label>
+        <label htmlFor="description">
+          Description
+          <textarea
+            id="description"
+            name="description"
+            placeholder="Description"
+            value={inputs.description}
+            onChange={handleChange}
+          />
+        </label>
+      </fieldset>
+      <button type="submit">+ Add Product</button>
+    </Form>
+  );
+}
